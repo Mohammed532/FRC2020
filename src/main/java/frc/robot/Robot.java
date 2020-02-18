d9e75bc641f4723faa725062c3dc2a143a551448
DqddyMike <mukenye2@gmail.com>
2020-02-18 17:33:09 -0500

    Teleop Code Stuff

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,12 +5,16 @@ import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
+import edu.wpi.first.wpilibj.GenericHID;
 
 public class Robot extends TimedRobot {
+  
+  double Speed;
+  
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
@@ -18,13 +22,10 @@ public class Robot extends TimedRobot {
   private final DifferentialDrive robotDrive = new DifferentialDrive(new Spark(0), new Spark(1)); //Sparks have placeholder values
   private final XboxController c_xbox = new XboxController(0); //Xbox controller port is a placeholder
   private final Timer timer = new Timer();
+  private final DifferentialDrive ShooterWheels = new DifferentialDrive(new Spark(2), new Spark(3)); 
+  private final Spark InWheels = new Spark(4); //SUCC=INTAKE WHEELS
 
-  private double intSpeed;
 
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
   @Override
   public void robotInit() {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
@@ -80,152 +81,73 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    intSpeed = 0.5;
   }
 
   /**
    * This function is called periodically during operator control.
    */
   @Override
-  public void teleopPeriodic() {
-    boolean yButtonPressed = c_xbox.getYButtonPressed();
-    boolean bbuttonPressed = c_xbox.getBButtonPressed();
-    boolean abuttonPressed = c_xbox.getAButtonPressed(); 
-    double lXAxis = c_xbox.getRawAxis(1);
-    double lYAxis = c_xbox.getRawAxis(0);
+  public void teleopPeriodic() { 
+    double lXAxis = c_xbox.getRawAxis(0);
+    double lYAxis = c_xbox.getRawAxis(1);
+    double RT = c_xbox.getTriggerAxis(Hand.kRight);
+    boolean RB = c_xbox.getBumper(Hand.kRight);
+    double rXAxis = c_xbox.getRawAxis(4);
+    // double rYAxis = c_xbox.getRawAxis(5); 
+    double speedMod = getspeedMod(c_xbox);
 
-    boolean abutton = false;
-    boolean bbutton = false;
-    boolean ybutton = false;
+    robotDrive.arcadeDrive(lYAxis * speedMod, rXAxis * speedMod);
+    //placeholder values por outake :)
 
-    double speedMod = intSpeed;
-    // String anyButtonPressed = "";
-
-    if (yButtonPressed){
-      ybutton = true;
-      bbutton = false;
-      abutton = false;
-    } else if (bbuttonPressed){
-      ybutton = false;
-      bbutton = true;
-      abutton = false;
-    } else if (abuttonPressed){
-      ybutton = false;
-      bbutton = false;
-      abutton = true;
+    if (RT > 0.35){
+      ShooterWheels.arcadeDrive(1.0, 0);
     }
 
-    if(ybutton) {
-      
-      System.out.println ("Y button is pressed female dog");
-      speedMod = 0.75;
-  
+    if (RB){
+      ShooterWheels.arcadeDrive(-1.0, 0);
+      InWheels.set(-1.0);
+
     }
-    if(bbutton) { 
-      
-      System.out.println ("B button is pressed coronavirus");
-      speedMod = 0.50;
-  
-    }
-    if(abutton) {
-      
-      System.out.println ("A button is pressed black boy");
-      speedMod = 0.25;
-  
-    }
-
-    robotDrive.arcadeDrive(lXAxis * speedMod,lYAxis * speedMod);
-
-    /*if (ybutton || bbutton || abutton){
-      anyButtonPressed = "true";
-    }*/
-
-    /*if (anyButtonPressed != "true"){
-      speedMod = intSpeed;
-      robotDrive.arcadeDrive(lXAxis * speedMod,lYAxis * speedMod);
-
-    }else if (anyButtonPressed == "true"){
-      
-      if(ybutton) {
-      
-        System.out.println ("Y button is pressed female dog");
-        speedMod = 0.75;
     
-      }
-      if(bbutton) { 
-        
-        System.out.println ("B button is pressed coronavirus");
-        speedMod = 0.50;
-    
-      }
-      if(abutton) {
-        
-        System.out.println ("A button is pressed black boy");
-        speedMod = 0.25;
-    
-      }
-
-      robotDrive.arcadeDrive(lXAxis * speedMod,lYAxis * speedMod);
-
-    }*/
-    
-
-    /*if(ybutton) {
-      
-      System.out.println ("Y button is pressed female dog");
-      speedMod = 0.75;
-  
-    }
-    if(bbutton) { 
-      
-      System.out.println ("B button is pressed coronavirus");
-      speedMod = 0.50;
-  
-    }
-    if(abutton) {
-      
-      System.out.println ("A button is pressed black boy");
-      speedMod = 0.25;
-  
-    }*/
-
-    // robotDrive.arcadeDrive(lXAxis * speedMod,lYAxis * speedMod);
 
    // m_myRobot.arcadeDrive(m_leftStick.getY(), m_rightStick.getY(Hand.kRight));
   }
 
-  /*public double getspeedMod(boolean AButton, boolean BButton, boolean YButton) {
-    boolean ybutton = c_xbox.getYButton();
-    boolean bbutton = c_xbox.getBButton();
-    boolean abutton = c_xbox.getAButton();
-  
-    boolean yButtonPressed = YButton;
-    boolean bButtonPressed = BButton;
-    boolean aButtonPressed = AButton;
+  public double getspeedMod(XboxController xbox) {
+    boolean ybutton = xbox.getYButton();
+    //boolean bbutton = xbox.getBButton();
+    //boolean abutton = xbox.getAButton();
 
+    boolean yButtonPressedTwice = false;
 
-    if(yButtonPressed) {
+    if(ybutton && !yButtonPressedTwice) {
       
-      System.out.println ("Y button is pressed female dog");
-      intSpeed = 0.75;
-  
+      // System.out.println ("Y button is pressed female dog");
+      Speed = 0.75;
+      yButtonPressedTwice = true;
+
+    }else if (ybutton && yButtonPressedTwice){
+      // System.out.println ("ybutton2x");
+      Speed = 0.5;
+      yButtonPressedTwice = false;
     }
-    if(bButtonPressed) { 
+
+   /* if(bbutton) { 
       
       System.out.println ("B button is pressed coronavirus");
-      intSpeed = 0.50;
+      Speed = 0.50;
   
     }
-    if(aButtonPressed) {
+    if(abutton) {
       
       System.out.println ("A button is pressed black boy");
-      intSpeed = 0.25;
+      Speed = 0.25;
   
-    }
+    } */
 
-    return intSpeed;
+    return Speed; 
 
-  }*/
+  }
   
 
   /**
@@ -233,5 +155,9 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void testPeriodic() {
+ 
   }
+
+
+
 }
